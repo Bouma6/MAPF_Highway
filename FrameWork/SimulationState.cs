4124d98b9e9320696e0506666501be1e5da34405
Bouma <Christoferbouma@gmail.com>
2025-07-15 18:00:34 +0200

    Simulation state basis

--- FrameWork/SimulationState.cs
+++ FrameWork/SimulationState.cs
@@ -2,5 +2,13 @@ namespace FrameWork;
 
 public class SimulationState
 {
-    
+    public Map Map;
+    public TaskMaster TaskMaster;
+    public RobotMaster RobotMaster;
+    public SimulationState(string mapName,string taskName,string robotName)
+    {
+        Map = new Map(mapName);
+        TaskMaster = new TaskMaster(taskName);
+        RobotMaster = new RobotMaster(robotName);
+    }
 }
