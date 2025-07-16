39b2fd5f6e94252de51a2822c24df52702ad4f52
Bouma <Christoferbouma@gmail.com>
2025-07-16 15:50:19 +0200

    Implementation of simulator

--- Simulator/SimulationRunner.cs
+++ Simulator/SimulationRunner.cs
@@ -1,6 +1,36 @@
 namespace MAPF_Highway;
+using Planner;
+using FrameWork;
 
 public class SimulationRunner
 {
-    
+    private readonly TimeSpan _interval =TimeSpan.FromSeconds(1);
+    private readonly SimulationEngine _simulationEngine;
+    public SimulationRunner()
+    {
+        Planner planner = new();
+        _simulationEngine = new SimulationEngine(planner,Config.MapName,Config.RobotName,Config.TaskName);
+        
+    }
+
+    public async Task RunAsync(int steps=10)
+    {
+        _simulationEngine.StartPlanner();
+        for (int step = 0; step < steps; step++)
+        {
+            var start = DateTime.UtcNow;
+            _simulationEngine.Tick();
+            
+            var elapsed = DateTime.UtcNow - start;
+            var delay = _interval - elapsed;
+
+            if (delay > TimeSpan.Zero)
+                await Task.Delay(delay);
+            else
+                Console.WriteLine($"⚠️ Step {step} overran the interval.");
+        }
+
+        Console.WriteLine("✅ PlanScheduler completed all steps.");
+    }
 }
+    
