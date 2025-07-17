5c8be63c274c54573c4f92555d7d9fb9040bc6ad
Bouma <Christoferbouma@gmail.com>
2025-07-17 09:44:52 +0200

    Added reference to avalonia for future Simulation display

--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -1,12 +1,10 @@
 ﻿namespace MAPF_Highway;
-//Implement the fucking timer for 1 second
 //Print out the game map 
 class Program
 {
     public static void Main(string[] args)
     {
         SimulationRunner runner = new();
-        
         Task.Run(() => runner.RunAsync());
     }
 }
