136538b4cd728da114b72f80ae9c11933ae6b5aa
Bouma <Christoferbouma@gmail.com>
2025-07-20 09:32:26 +0200

    Added implementation of PlannerState

--- Planner/PlannerState.cs
+++ Planner/PlannerState.cs
@@ -1,6 +1,15 @@
 namespace Planner;
-
+using FrameWork;
 public class PlannerState
 {
-    
+    public Map Map;
+    public TaskMaster TaskMaster;
+    public RobotMaster RobotMaster;
+    public PlannerState(string mapName,string taskName,string robotName)
+    {
+        Map = new Map(mapName);
+        TaskMaster = new TaskMaster(taskName);
+        RobotMaster = new RobotMaster(robotName);
+
+    }
 }
