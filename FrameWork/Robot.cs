167b64ee09bdb927f9dc5e52fa0cbb4cedf66041
Bouma <Christoferbouma@gmail.com>
2025-07-20 18:35:01 +0200

    setter needs to be public

--- FrameWork/Robot.cs
+++ FrameWork/Robot.cs
@@ -3,7 +3,7 @@ namespace FrameWork;
 public class Robot(RobotId robotId,Position position)
 {
     public RobotId RobotId { get;} = robotId;
-    public Position Position { get; private set; } = position;
+    public Position Position { get;  set; } = position;
     public RobotTask? CurrentTask { get; private set ; }
 
     public void Move(Direction direction)
