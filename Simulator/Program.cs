104fc470f9b4bea2c841eb4289e04191d1fdd054
Bouma <Christoferbouma@gmail.com>
2025-07-20 18:34:48 +0200

    Rewritten to avalonia standards

--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -1,35 +1,24 @@
-﻿using FrameWork;
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using System;
+using System.Threading.Tasks;
+using Avalonia.Controls;
 
 namespace MAPF_Highway;
+
 class Program
 {
+    [STAThread]
     public static void Main(string[] args)
     {
-        //Start Avalonia application that displays the current map state
-        Task.Run(ConsoleRenderer.StartAvalonia);
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args, ShutdownMode.OnMainWindowClose);
+    }
 
-        //Start the program with the planner 
-        SimulationRunner runner = new();
-        Task.Run(() => runner.RunAsync(Config.Steps));
-
-        Console.ReadLine();
-        
-        /*
-        TaskMaster taskMaster = new TaskMaster(Config.TaskName,256);
-        for (int i = 0; i < 5; i++)
-        {
-            Console.WriteLine(taskMaster[i].Pickup.y);
-            Console.WriteLine(taskMaster[i].Pickup.x);
-            Console.WriteLine(taskMaster[i].Destination.y);
-            Console.WriteLine(taskMaster[i].Destination.x);
-        }
-
-        Map map = new Map(Config.MapName);
-        Position pos = new Position(0, 1);
-        map[1, 0] = MapSymbols.Destination;
-        map[pos] = MapSymbols.Robot;
-        map[pos.Move(Direction.Down)] = MapSymbols.Pickup;
-        Console.WriteLine(map);
-        */
+    private static AppBuilder BuildAvaloniaApp()
+    {
+        return AppBuilder.Configure<MapApp>()
+            .UsePlatformDetect()
+            .LogToTrace();
     }
 }
